28d00322335eeafc47f80582d59179ae036fc058
Daniel Costa <dancosta.id@gmail.com>
2022-02-22 16:23:37 -0300

    Finished all the backend

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   get 'tasks', to: 'tasks#index', as: :tasks
   get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
